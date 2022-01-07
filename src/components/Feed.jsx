5c1abeb6140d9ffa2017f0f1dc32d0db371824c4
김하경 <khg05311@naver.com>
2022-01-07 18:13:34 +0900

    2:13:04

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,16 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import {useParams } from 'react-router-dom'
 
-import {client} from '../client'
+import { client } from '../client'
+import { searchQuery } from '../utils/data'
 import MasonryLayout from './MasonryLayout'
 import Spinner from './Spinner'
 
 const Feed = () => {
-const [loading, setLoading] = useState(false)
-const { category} = useParams()
+  const [loading, setLoading] = useState(false)
+  const [pins, setPins] = useState()
+const { categoryId } = useParams()
 
 useEffect(() => {
-  
+  if (categoryId) { 
+    setLoading(true)
+
+    if (categoryId) {
+      const query = searchQuery(categoryId)
+
+      client.fetch(query)
+        .then((data) => {
+          setPins(data)
+          setLoading(false)
+        })
+    }
+  } else {
+
+  }
 }, [])
 
 if(loading) return <Spinner message='we are adding new ideas to your feed!' />
